8501c71a0f8e5b0351abb36fc9e6a814c67405b7
Nick Janovetz <cubbix1@gmail.com>
2015-01-26 19:57:07 -0600

    Constructor updates, non speedcontroller passable

--- src/DriveMain.java
+++ src/DriveMain.java
@@ -1,12 +1,14 @@
-import edu.wpi.first.wpilibj.Joystick;
+//import edu.wpi.first.wpilibj.Joystick;
 
 public class DriveMain {
 
 	public static void main(String[] args) {
-	
-		Joystick stick = new Joystick(0);
-		
-		SwerveDrive d1 = new SwerveDrive();
-		d1.updateSwerve(stick.getY(), stick.getX(), stick.getZ()); //for,strafe,rotCW
+
+		//Joystick stick = new Joystick(0);
+
+		SwerveDrive d1 = new SwerveDrive(36, 24);
+		//while (stick.getTrigger() == true) {
+			d1.updateSwerve(.5, -.2, 1); // for,strafe,rotCW
+		//}
 	}
 }
--- src/SwerveDrive.java
+++ src/SwerveDrive.java
@@ -1,4 +1,4 @@
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SwerveDrive {
 
@@ -13,6 +13,8 @@ public class SwerveDrive {
 
 	double width, length;
 
+	// SpeedController TR, TL, BR, BL;
+
 	private double strafe, forward, rotationCW, Rx, Ry;
 	private double topX, bottomX, rightY, leftY;
 	private double topRightSpeed, topLeftSpeed, bottomLeftSpeed,
@@ -22,10 +24,17 @@ public class SwerveDrive {
 
 	// CONSTRUCTOR
 
-	public SwerveDrive(double length, double width, SpeedController TR,
-			SpeedController TL, SpeedController BL, SpeedController BR) {
+	public SwerveDrive(double length, double width/*
+												 * , SpeedController TR,
+												 * SpeedController TL,
+												 * SpeedController BL,
+												 * SpeedController BR
+												 */) {
 		this.width = width;
 		this.length = length;
+
+		// this.TR = TR; this.TL = TL; this.BL = BL; this.BR = BR;
+
 	}
 
 	// INTERFACE METHODS
@@ -33,25 +42,35 @@ public class SwerveDrive {
 			double rotationIn) {
 		forward = forwardIn;
 		strafe = strafeIn;
-		rotationCW = rotationIn;// * 2 * Math.PI;
+		rotationCW = rotationIn * 2 * Math.PI;
 
-		this.Calc();
+		this.calc();
 		// System.out output
-		// System.out.println(topRightSpeed + "\n" + topLeftSpeed + "\n"
-		// + bottomLeftSpeed + "\n" + bottomRightSpeed + "\n");
-		// System.out.println(topRightAngle + "\n" + topLeftAngle + "\n"
-		// + bottomLeftAngle + "\n" + bottomRightAngle + "\n");
+		System.out.println(topRightSpeed + "\n" + topLeftSpeed + "\n"
+				+ bottomLeftSpeed + "\n" + bottomRightSpeed + "\n");
+		System.out.println(topRightAngle + "\n" + topLeftAngle + "\n"
+				+ bottomLeftAngle + "\n" + bottomRightAngle + "\n");
 
 		// SmartDashboard output
-		SmartDashboard.putNumber("Top Right Speed", topRightSpeed);
-		SmartDashboard.putNumber("Top Left Speed", topRightSpeed);
-		SmartDashboard.putNumber("Bottom Left Speed", topRightSpeed);
-		SmartDashboard.putNumber("Bottom Right Speed", topRightSpeed);
+		/*
+		 * SmartDashboard.putNumber("Top Right Speed", topRightSpeed);
+		 * SmartDashboard.putNumber("Top Left Speed", topRightSpeed);
+		 * SmartDashboard.putNumber("Bottom Left Speed", topRightSpeed);
+		 * SmartDashboard.putNumber("Bottom Right Speed", topRightSpeed);
+		 * 
+		 * SmartDashboard.putNumber("Top Right Angle", topRightAngle);
+		 * SmartDashboard.putNumber("Top Left Angle", topRightAngle);
+		 * SmartDashboard.putNumber("Bottom Left Angle", topRightAngle);
+		 * SmartDashboard.putNumber("Bottom Right Angle", topRightAngle);
+		 */
 
-		SmartDashboard.putNumber("Top Right Angle", topRightAngle);
-		SmartDashboard.putNumber("Top Left Angle", topRightAngle);
-		SmartDashboard.putNumber("Bottom Left Angle", topRightAngle);
-		SmartDashboard.putNumber("Bottom Right Angle", topRightAngle);
+		// Wheel Output
+		// TR.set(topRightSpeed);
+		// TL.set(topLeftSpeed);
+		// BL.set(bottomLeftSpeed);
+		// BR.set(bottomRightSpeed);
+		//
+		//
 	}
 
 	public void setStrafe(double strafeIn) {
@@ -96,8 +115,8 @@ public class SwerveDrive {
 	}
 
 	private void setRotationVectors() {
-		Rx = rotationCW * LENGTH / 2;
-		Ry = rotationCW * WIDTH / 2;
+		Rx = rotationCW * length / 2;
+		Ry = rotationCW * width / 2;
 	}
 
 	private void setIntermediates() {
@@ -106,8 +125,6 @@ public class SwerveDrive {
 		bottomX = strafe - Rx;
 		leftY = strafe + Ry;
 
-		System.out.println(bottomX + "\n" + topX + "\n" + rightY + "\n" + leftY
-				+ "\n");
 	}
 
 	private void setWheelSpeeds() {
@@ -117,6 +134,23 @@ public class SwerveDrive {
 		bottomRightSpeed = Math
 				.sqrt(Math.pow(bottomX, 2) + Math.pow(rightY, 2));
 
+		double max;
+		max = topRightSpeed;
+		if (topLeftSpeed > max) {
+			max = topLeftSpeed;
+		}
+		if (bottomLeftSpeed > max) {
+			max = bottomLeftSpeed;
+		}
+		if (bottomRightSpeed > max) {
+			max = bottomRightSpeed;
+		}
+		if (max > 1) {
+			topRightSpeed /= max;
+			topLeftSpeed /= max;
+			bottomLeftSpeed /= max;
+			bottomRightSpeed /= max;
+		}
 	}
 
 	private void setWheelAngles() {
@@ -126,7 +160,7 @@ public class SwerveDrive {
 		bottomRightAngle = 90 - Math.atan2(bottomX, rightY) * 180 / Math.PI;
 	}
 
-	private void Calc() {
+	private void calc() {
 		this.normalizeTranslation();
 		this.setRotationVectors();
 		this.setIntermediates();
